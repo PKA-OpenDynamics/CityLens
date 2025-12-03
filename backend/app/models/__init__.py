5d950df6a579b058b71d23ef76c46235cd72edbe
Vu Dang Khoa <khoabuit0910@gmail.com>
2025-12-03 23:46:59 +0700

    feat(models): initialize models package

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2025 CityLens Contributors
-# Licensed under the GNU General Public License v3.0 (GPL-3.0)
-
-from app.models.user import User
-from app.models.report import Report, ReportStatus
-from app.models.incident import Incident, AlertLevel
