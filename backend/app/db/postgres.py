a06ba29af0b36f0163d37d61f0196221233100e1
Vu Dang Khoa <khoabuit0910@gmail.com>
2025-12-07 22:44:28 +0700

    refactor(db): improve postgres connection

--- backend/app/db/postgres.py
+++ backend/app/db/postgres.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URI,
+    settings.SQLALCHEMY_SYNC_DATABASE_URI,
     pool_pre_ping=True,
     echo=settings.LOG_LEVEL == "DEBUG"
 )
