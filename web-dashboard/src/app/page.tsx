29da37bcfe30a9d5d0c507fccab91bfc2429506a
Vu Dang Khoa <khoabuit0910@gmail.com>
2025-12-07 22:56:09 +0700

    feat(web): add app layout and main page

---
+++ web-dashboard/src/app/page.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) 2025 CityLens Contributors
+// Licensed under the GNU General Public License v3.0 (GPL-3.0)
+
+import { redirect } from 'next/navigation';
+
+export default function Home() {
+  redirect('/dashboard');
+}
